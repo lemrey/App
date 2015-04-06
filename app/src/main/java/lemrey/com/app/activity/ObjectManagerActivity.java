2acbff67815d6efd33b11ce0cc4af82dcae5ef05
lemrey <lemrey@gmail.com>
2015-04-06 19:43:56 +0200

    Changed to use LocalBroadcasts

--- app/src/main/java/lemrey/com/app/activity/ObjectManagerActivity.java
+++ app/src/main/java/lemrey/com/app/activity/ObjectManagerActivity.java
@@ -1,160 +1,66 @@
 package lemrey.com.app.activity;
 
-import android.content.ComponentName;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
-
-import java.util.ArrayList;
 
 import lemrey.com.app.DeviceRegister;
 import lemrey.com.app.R;
 import lemrey.com.app.adapter.DeviceDetailsAdapter;
-import lemrey.com.app.device.Device;
 import lemrey.com.app.service.ConnectionService;
-import lemrey.com.app.service.DeviceCallback;
 
 
-public class ObjectManagerActivity extends ActionBarActivity implements DeviceCallback {
+public class ObjectManagerActivity extends ActionBarActivity /*implements DeviceCallback*/ {
 
 	private static final String TAG = "ObjectManagerActivity";
-
-	private ConnectionService mConnectionService;
-	private boolean mIsBoundToService = false;
-
-	//private DeviceRegister mDeviceRegister;
+	private final DeviceStatus mDeviceCallbacks = new DeviceStatus();
+	private LocalBroadcastManager mBroadcastManager;
+	private ExpandableListView mListView;
 	private DeviceDetailsAdapter mListAdapter;
 
-	private ExpandableListView mListView;
-
-	private ServiceConnection mLocalServiceConnection = new ServiceConnection() {
-
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			Log.d(TAG, "Attached to service");
-			mIsBoundToService = true;
-			mConnectionService = ((ConnectionService.EventServiceBinder) service).getService();
-			//initiateConnections();
-
-			ArrayList<Device> m = new ArrayList<>();
-			m.addAll(DeviceRegister.devices());
-			mListAdapter = new DeviceDetailsAdapter(ObjectManagerActivity.this,	m);
-			mListView.setAdapter(mListAdapter);
-
-			mConnectionService.startConnections(ObjectManagerActivity.this);
-		}
-
-		public void onServiceDisconnected(ComponentName name) {
-			Log.d(TAG, "Detached from service");
-		}
-
-	};
-
-	private void initiateConnections() {
-		Log.d(TAG, "Initiating connections");
-
-		/*for (Device device : mDeviceRegister) {
-			if (device.status() != Device.ConnectionStatus.CONNECTED) {
-				mConnectionService.startConnection(device.mAddress, this);
-				device.setConnecting();
-				mListAdapter.notifyDataSetChanged();
-			}
-		}*/
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_object_manager);
-
 		setTitle("Objects");
 
-		//mDeviceRegister = new DeviceRegister();
-		//mListAdapter = new DeviceAdapter(this);
-		//mListAdapter = new DeviceDetailsAdapter(this, mConnectionService.devices());
-
-		//mListAdapter.setNotifyOnChange(true);
-
+		mBroadcastManager = LocalBroadcastManager.getInstance(this);
 		mListView = (ExpandableListView) findViewById(R.id.listObjects);
 		mListView.setChoiceMode(ListView.CHOICE_MODE_NONE);
-
-		/*RuleBook.addRule(new Rule("00:06:66:61:DB:DD", new Event("button on"),
-				"00:06:66:61:DB:DD", new Command("led toggle")));*/
-
-}
+		mListAdapter = new DeviceDetailsAdapter(this, DeviceRegister.devices());
+		mListView.setAdapter(mListAdapter);
+		// Start the connection service
+		Intent intent = new Intent(this, ConnectionService.class);
+		startService(intent);
+	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//Log.d(TAG, "On Resume");
-		if (!mIsBoundToService) {
-			bindLocalService();
-		}
+		mBroadcastManager.registerReceiver(mDeviceCallbacks, new IntentFilter("bing"));
 	}
 
 	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		//Log.d(TAG, "On Destroy");
-		if (mIsBoundToService) {
-			mIsBoundToService = false;
-			unbindService(mLocalServiceConnection);
+	protected void onPause() {
+		super.onPause();
+		mBroadcastManager.unregisterReceiver(mDeviceCallbacks);
+	}
+
+	private class DeviceStatus extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals("bing")) {
+				//Log.d(TAG, "connected");
+				mListAdapter.notifyDataSetChanged();
+				// Do stuff - maybe update my view based on the changed DB contents
+			}
 		}
-	}
-
-	private void bindLocalService() {
-		if (!ConnectionService.isRunning()) {
-			startService(new Intent(this, ConnectionService.class));
-		} else {
-			Log.d(TAG, "Service running, just bind");
-		}
-		bindService(new Intent(this, ConnectionService.class),
-				mLocalServiceConnection, BIND_AUTO_CREATE);
-	}
-
-	public void onDeviceConnected(String deviceAddress) {
-		Log.d(TAG, "OnDeviceConnected");
-		mListAdapter.notifyDataSetChanged();
-
-	}
-
-	public void onDeviceDisconnected(String deviceAddress) {
-		Log.d(TAG, "OnDeviceDisconnected");
-		mListAdapter.notifyDataSetChanged();
-	}
-
-	@Override
-	public void onPayloadReceived(String deviceAddress) {
-		Log.d(TAG, "OnPayloadReceived");
-		mListAdapter.notifyDataSetChanged();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_object_manager, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			mConnectionService.writeSomething("{\"msg\":\"cmd\", \"command\": {\"name\":\"led toggle\"}}");
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
