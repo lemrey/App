d769139423b0e3812aba2aa9ab07e8ff22a95d63
lemrey <lemrey@gmail.com>
2015-04-08 21:41:39 +0200

    Now uses DeviceRegister as backing data source. Updated icons.

--- app/src/main/java/lemrey/com/app/adapter/DeviceDetailsAdapter.java
+++ app/src/main/java/lemrey/com/app/adapter/DeviceDetailsAdapter.java
@@ -2,17 +2,15 @@ package lemrey.com.app.adapter;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
+import lemrey.com.app.DeviceRegister;
 import lemrey.com.app.R;
 import lemrey.com.app.device.Device;
 import lemrey.com.app.device.Feature;
@@ -29,78 +27,19 @@ public class DeviceDetailsAdapter extends BaseExpandableListAdapter {
 	private final List<Device> mDevices;
 	private final LayoutInflater mLayoutInflater;
 
-	public DeviceDetailsAdapter(Activity activity, Collection<Device> devices) {
+	public DeviceDetailsAdapter(Activity activity) {
 		mActivity = activity;
-		mDevices = new ArrayList<>(devices);
+		mDevices = DeviceRegister.devices();
 		mLayoutInflater = activity.getLayoutInflater();
 	}
 
 	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		Device dev = mDevices.get(groupPosition);
-		return dev.features().get(childPosition);
-		//return groups.get(groupPosition).children.get(childPosition);
-	}
-
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return 0;
-	}
-
-	@Override
-	public View getChildView(int groupPosition, final int childPosition,
-	                         boolean isLastChild, View convertView, ViewGroup parent) {
-		if (convertView == null) {
-			convertView = mLayoutInflater.inflate(R.layout.list_object_details, null);
-		}
-		final String name;
-		//final Object child = getChild(groupPosition, childPosition);
-		Feature child = mDevices.get(groupPosition).features().get(childPosition);
-		final TextView label = (TextView) convertView.findViewById(R.id.textView1);
-		final Drawable iconDirection;
-		final Drawable iconData;
-
-		name = child.name;
-		Log.d("adapterzzz", name + " is a " + child.type);
-		if (child.type.equals(FeatureType.EVENT)) {
-			iconDirection = mActivity.getResources().getDrawable(R.drawable.ic_event);
-		} else {
-			iconDirection = mActivity.getResources().getDrawable(R.drawable.ic_cmd);
-		}
-		switch (child.paramType) {
-			default:
-				iconData = mActivity.getResources().getDrawable(R.drawable.ic_bubble);
-				break;
-			case NUMBER:
-				iconData = mActivity.getResources().getDrawable(R.drawable.ic_dice);
-				break;
-			case TEXT:
-				iconData = mActivity.getResources().getDrawable(R.drawable.ic_paperplane);
-				break;
-		}
-
-		iconDirection.setBounds(0, 0, 64, 64);
-		iconData.setBounds(0, 0, 64, 64);
-
-
-		label.setText(name);
-
-		/*convertView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(mActivity, children,
-						Toast.LENGTH_SHORT).show();
-			}
-		});*/
-
-
-		label.setCompoundDrawablesRelative(iconDirection, null, iconData, null);
-		return convertView;
+	public int getGroupCount() {
+		return mDevices.size();
 	}
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
-		//return groups.get(groupPosition).children.size();
 		return mDevices.get(groupPosition).features().size();
 	}
 
@@ -110,8 +49,9 @@ public class DeviceDetailsAdapter extends BaseExpandableListAdapter {
 	}
 
 	@Override
-	public int getGroupCount() {
-		return mDevices.size();
+	public Object getChild(int groupPosition, int childPosition) {
+		final Device dev = mDevices.get(groupPosition);
+		return dev.features().get(childPosition);
 	}
 
 	@Override
@@ -120,29 +60,36 @@ public class DeviceDetailsAdapter extends BaseExpandableListAdapter {
 	}
 
 	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return 0;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return false;
+	}
+
+	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 	                         View convertView, ViewGroup parent) {
-
 		View rowView = mLayoutInflater.inflate(R.layout.list_objects_entry, parent, false);
-		TextView labelName = (TextView) rowView.findViewById(R.id.labelName);
-
+		final TextView labelName = (TextView) rowView.findViewById(R.id.labelName);
 		// Set text and icon
-		Device device = mDevices.get(groupPosition);
+		final Device device = mDevices.get(groupPosition);
 		labelName.setText(device.name());
 
-		Drawable img = null;
+		final Drawable img;
 		switch (device.status()) {
-			case DISCONNECTED:
-				img = mActivity.getResources().getDrawable(R.drawable.ic_disconnected);
+			case CONNECTED:
+				img = mActivity.getResources().getDrawable(R.drawable.ic_connected);
 				break;
 			case CONNECTING:
 				img = mActivity.getResources().getDrawable(R.drawable.ic_connecting);
 				break;
-			case CONNECTED:
-				img = mActivity.getResources().getDrawable(R.drawable.ic_connected);
+			default:
+				img = mActivity.getResources().getDrawable(R.drawable.ic_disconnected);
 				break;
 		}
-
 		img.setBounds(0, 0, 64, 64);
 		labelName.setCompoundDrawablesRelative(null, null, img, null);
 
@@ -150,8 +97,41 @@ public class DeviceDetailsAdapter extends BaseExpandableListAdapter {
 	}
 
 	@Override
-	public boolean hasStableIds() {
-		return false;
+	public View getChildView(int groupPosition, final int childPosition,
+	                         boolean isLastChild, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = mLayoutInflater.inflate(R.layout.list_object_details, null);
+		}
+		final String name;
+		final Feature child = mDevices.get(groupPosition).features().get(childPosition);
+		final TextView label = (TextView) convertView.findViewById(R.id.textView1);
+		final Drawable iconDirection;
+		final Drawable iconData;
+
+		name = child.name;
+		//Log.d("adapterzzz", name + " is a " + child.type);
+		if (child.type.equals(FeatureType.EVENT)) {
+			iconDirection = mActivity.getResources().getDrawable(R.drawable.ic_event);
+		} else {
+			iconDirection = mActivity.getResources().getDrawable(R.drawable.ic_cmd);
+		}
+		switch (child.paramType) {
+			default:
+				iconData = mActivity.getResources().getDrawable(R.drawable.ic_signal);
+				break;
+			case NUMBER:
+				iconData = mActivity.getResources().getDrawable(R.drawable.ic_pi);
+				break;
+			case TEXT:
+				iconData = mActivity.getResources().getDrawable(R.drawable.ic_paperplane);
+				break;
+		}
+		iconDirection.setBounds(0, 0, 64, 64);
+		iconData.setBounds(0, 0, 48, 48);
+
+		label.setText(name);
+		label.setCompoundDrawablesRelative(iconDirection, null, iconData, null);
+		return convertView;
 	}
 
 	@Override
