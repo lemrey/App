0da52ffaabf9d6951c1f671088589871755a342c
lemrey <lemrey@gmail.com>
2015-05-01 19:38:07 +0200

    Converted to ArrayAdapter. Switch now working

--- app/src/main/java/lemrey/com/app/adapter/RuleAdapter.java
+++ app/src/main/java/lemrey/com/app/adapter/RuleAdapter.java
@@ -1,97 +1,48 @@
 package lemrey.com.app.adapter;
 
 import android.content.Context;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.List;
 
-import lemrey.com.app.DeviceRegister;
 import lemrey.com.app.R;
 import lemrey.com.app.rule.Rule;
 
 /**
- * Created by lemrey on 3/29/2015.
+ * Created by lemrey on 5/1/2015.
  */
-public class RuleAdapter extends BaseExpandableListAdapter {
+public class RuleAdapter extends ArrayAdapter<Rule> {
 
 	private final List<Rule> mRules;
 	private final LayoutInflater mLayoutInflater;
 
-	public RuleAdapter(Context context, List<Rule> rules) {
-		mRules = rules;
-		mLayoutInflater = (LayoutInflater)
-				context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	public RuleAdapter(Context context, int resource, List<Rule> objects) {
+		super(context, resource, objects);
+		mRules = objects;
+		mLayoutInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
 	@Override
-	public int getGroupCount() {
-		return mRules.size();
-	}
-
-	@Override
-	public int getChildrenCount(int i) {
-		return 2;
-	}
-
-	@Override
-	public Object getGroup(int i) {
-		return mRules.get(i);
-	}
-
-	@Override
-	public Object getChild(int i, int i2) {
-		return i2 == 0 ? mRules.get(i).event : mRules.get(i).cmd;
-	}
-
-	@Override
-	public long getGroupId(int i) {
-		return 0;
-	}
-
-	@Override
-	public long getChildId(int i, int i2) {
-		return 0;
-	}
-
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
-
-	@Override
-	public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+	public View getView(int position, View view, ViewGroup parent) {
 		if (view == null) {
-			view = mLayoutInflater.inflate(R.layout.list_rule_entry, null);
+			view = mLayoutInflater.inflate(R.layout.list_rule_entry, parent, false);
 		}
-		final String ruleName = mRules.get(i).event.name;
-		final TextView labelRuleName = (TextView) view.findViewById(R.id.labelName);
-		labelRuleName.setText(ruleName);
-		return view;
-	}
+		final TextView label = (TextView) view.findViewById(R.id.labelName);
+		final Switch switchEnabled = (Switch) view.findViewById(R.id.switchEnabled);
+		final Rule rule = mRules.get(position);
 
-	@Override
-	public View getChildView(int i, int i2, boolean b, View view, ViewGroup viewGroup) {
-		if (view == null) {
-			view = mLayoutInflater.inflate(R.layout.list_rule_details, null);
-		}
-		final String childName;
-		final Rule rule = mRules.get(i);
-		if (i2 == 0) {
-			childName = "when " + DeviceRegister.device(rule.srcAddr).name() + "->" + rule.event.name;
-		} else {
-			childName = "then " + DeviceRegister.device(rule.destAddr).name() + "->" + rule.cmd.name;
-		}
-		final TextView labelRuleName = (TextView) view.findViewById(R.id.labelName);
-		labelRuleName.setText(childName);
-		return view;
-	}
+		label.setText("if \t\t\t" + rule.event.name + "\nthen\t" + rule.cmd.name);
+		label.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+		switchEnabled.setChecked(rule.isEnabled());
 
-	@Override
-	public boolean isChildSelectable(int i, int i2) {
-		return false;
+		return view;
 	}
 }
+
