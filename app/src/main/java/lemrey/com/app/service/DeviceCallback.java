592a4c27cbd53dc536e46ffb8784a9a89d236d5c
lemrey <lemrey@gmail.com>
2015-05-01 19:46:22 +0200

    Updating project

--- app/src/main/java/lemrey/com/app/service/DeviceCallback.java
+++
@@ -1,7 +0,0 @@
-package lemrey.com.app.service;
-
-public interface DeviceCallback {
-	void onDeviceConnected(String deviceAddress);
-	void onDeviceDisconnected(String deviceAddress);
-	void onPayloadReceived(String deviceAddress);
-}
